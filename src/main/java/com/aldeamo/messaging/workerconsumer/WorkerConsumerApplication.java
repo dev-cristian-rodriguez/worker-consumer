2be87de0af6d99a62d1514798bdca06c0b7ad4d6
Cristian <cuentagit1111@gmail.com>
2026-03-11 00:45:47 -0500

    first-commit

---
+++ src/main/java/com/aldeamo/messaging/workerconsumer/WorkerConsumerApplication.java
@@ -0,0 +1,13 @@
+package com.aldeamo.messaging.workerconsumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkerConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkerConsumerApplication.class, args);
+	}
+
+}
