d6711c460b274d9576d5b299e55a630b129074b3
Cristian <cuentagit1111@gmail.com>
2026-03-12 00:54:36 -0500

    feat: Add RabbitMQ configuration for messaging, retry, and dead-letter queues.

--- src/main/java/com/aldeamo/messaging/workerconsumer/config/RabbitMQConfig.java
+++ src/main/java/com/aldeamo/messaging/workerconsumer/config/RabbitMQConfig.java
@@ -3,8 +3,7 @@ package com.aldeamo.messaging.workerconsumer.config;
 import org.springframework.amqp.core.*;
 import org.springframework.amqp.rabbit.config.SimpleRabbitListenerContainerFactory;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
-import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
-import org.springframework.amqp.support.converter.MessageConverter;
+import org.springframework.amqp.support.converter.SimpleMessageConverter;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -79,15 +78,10 @@ public class RabbitMQConfig {
     }
 
     @Bean
-    public MessageConverter jsonMessageConverter() {
-        return new Jackson2JsonMessageConverter();
-    }
-
-    @Bean
     public SimpleRabbitListenerContainerFactory rabbitListenerContainerFactory(ConnectionFactory connectionFactory) {
         SimpleRabbitListenerContainerFactory factory = new SimpleRabbitListenerContainerFactory();
         factory.setConnectionFactory(connectionFactory);
-        factory.setMessageConverter(jsonMessageConverter());
+        factory.setMessageConverter(new SimpleMessageConverter());
         factory.setAcknowledgeMode(AcknowledgeMode.MANUAL);
         factory.setPrefetchCount(10);
         return factory;
